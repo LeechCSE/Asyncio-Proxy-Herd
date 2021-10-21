67f7ec6172ebc1f4326087ba529669ec16fe880c
LeechCSE <leechwoo613@gmail.com>
2021-10-21 15:41:14 +0900

    Update and refactor src codes for newest version of 3.10

---
+++ code/client_v2.py
@@ -0,0 +1,27 @@
+import config
+import asyncio
+import sys
+
+async def tcp_client(server_id, message):
+    reader, writer = await asyncio.open_connection(
+        config.LOCALHOST, config.PORTS[server_id])
+    
+    print('Sending ... {}'.format(message))
+    writer.write(message.encode())
+    
+    data = await reader.read(100)
+    print('Received: {}'.format(data.decode()))
+    
+    print('Close connection')
+    writer.close()
+    
+    
+def main():
+    server_id = sys.argv[1]
+    args = sys.argv[2:]
+    message = " ".join(args)
+    
+    asyncio.run(tcp_client(server_id, message))
+    
+if __name__ == '__main__':
+    main()
---
+++ code/server_v2.py
@@ -0,0 +1,63 @@
+import config
+import asyncio, aiohttp
+import sys, time
+
+def handle_IAMAT(args):
+    client_id = args[0]
+    loc = args[1]
+    time_recv = float(args[2])
+    
+    time_stamp = ''
+    time_diff = time.time() - time_recv
+    if time_diff >= 0.0:
+        time_stamp += '+'
+    time_stamp += '{:.9f}'.format(time_diff)
+    
+    return 'AT ' + " ".join([client_id, time_stamp, loc, args[2]])
+
+def handle_WHATSAT(args):
+    
+
+def make_resp_of(query):
+    tokens = query.split()
+    if tokens[0] == 'IAMAT':
+        return handle_IAMAT(tokens[1:])
+    elif tokens[0] == 'WHATSAT':
+        return handle_WHATSAT(tokens[1:])
+    else:
+        return '? ' + query
+        
+# client_connected_cb: called whenever a new client connection is established
+# takes two arguments, (reader, writer), which are intances of StreamReader and
+# StreamWriter classes
+# Since it is  a coroutine function, it is automatically scheduled as a Task
+async def client_call_back(reader, writer):
+    data = await reader.read(100)
+    query = data.decode()
+    
+    resp = make_resp_of(query)
+    
+    print('Sending: {}'.format(resp))
+    writer.write(resp.encode())
+    await writer.drain()
+    
+    print('Close the connection')
+    writer.close()
+
+async def run_server(server_id):
+    server = await asyncio.start_server(
+        client_call_back, config.LOCALHOST, config.PORTS[server_id])
+    
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print('Serving on {}'.format(addrs))
+
+    async with server:
+        await server.serve_forever()
+
+def main():
+    server_id = sys.argv[1]
+    
+    asyncio.run(run_server(server_id))
+
+if __name__ == '__main__':
+    main()
